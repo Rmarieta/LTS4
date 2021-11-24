5f155067943dc93da63fa741fa0961ccac590fd1
Rmarieta <raphael.marietan@epfl.ch>
2021-11-24 17:13:18 +0100

    Change to run.sh

--- upload_to_server/run.sh
+++ upload_to_server/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-#SBATCH --chdir /scratch/rmarieta
+#SBATCH --chdir /scratch/rmarieta/cluster_output
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 4096
 #SBATCH --time 00:30:00 
+#SBATCH --output test.out
 
 # Use modules to set the software environment
 module load gcc/8.4.0
 module load python/3.7.7
-python ../../home/rmarieta/upload_data.py # arg1 arg2
+python ../LTS4/data_preparation/test.py # arg1 arg2
+
+# To run : sbatch run.sh
