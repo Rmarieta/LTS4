13053baea44fbe9ec90acef74eb1fb8710eea341
Rmarieta <raphael.marietan@epfl.ch>
2022-01-07 17:30:51 +0100

    classifier.sh

--- scitas_run/classifier.sh
+++ scitas_run/classifier.sh
@@ -4,13 +4,12 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 40000
-#SBATCH --time 01:30:00 
+#SBATCH --time 00:30:00 
 #SBATCH --output ./cluster_output/classifier.out
 
 # Use modules to set the software environment
 module load gcc/8.4.0
 module load python/3.7.7
 source ../rma_env/bin/activate
-python -u classifier/graph_classifier.py --graph_dir './data/v1.5.2/graph_chopped_1_5' --algo 'logit' --seizure_types 'FNSZ' 'GNSZ'
-
+python -u classifier/graph_classifier.py --graph_dir './data/v1.5.2/graph_cov_1s' --algo 'logit' --seizure_types 'FNSZ' 'GNSZ' --cross_val False --is_cov True --plot False
 # To run : sbatch scitas_run/classifier.sh (in home/rmarieta/LTS4)
