db1b2e2489f8b2ec58471a9b393b4bc0acbfa5dd
Rmarieta <raphael.marietan@epfl.ch>
2022-01-11 17:57:04 +0100

    remove + rename files local

--- scitas_run/laplacian.sh
+++ scitas_run/laplacian.sh
@@ -2,16 +2,13 @@
 #SBATCH --chdir /home/rmarieta/LTS4
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 2
-#SBATCH --mem 40000
-#SBATCH --time 20:30:00 
-#SBATCH --output ./cluster_output/lapl.out
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 80000
+#SBATCH --time 12:30:00 
+#SBATCH --output ./cluster_output/lapl_low.out
 
 # Use modules to set the software environment
 module load gcc/8.4.0
 module load python/3.7.7
 source ../rma_env/bin/activate
-python -u learn_graph_laplacian/laplacian.py --graph_dir './data/v1.5.2/graph_1_5_60000'
-
-# To run : sbatch scitas_run/laplacian.sh (in home/rmarieta/LTS4)
-# 7h30 with 40000 of RAM and 1 node/task/cpu was just enough for 'dev' dataset (1/6 of 'train')
+python -u learn_graph_laplacian/laplacian_low_pass.py --graph_dir './data/v1.5.2/graph_lapl_low_50' --restrict_size 'False' --low_pass 'False' --seizure_types 'FNSZ' 'GNSZ'
