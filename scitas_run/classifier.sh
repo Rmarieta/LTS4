a3cb6edc0bdf664a8fe0b84f577bd8b36ca95d28
Rmarieta <raphael.marietan@epfl.ch>
2022-01-16 11:28:35 +0100

    graph_classifier remote

--- scitas_run/classifier.sh
+++ scitas_run/classifier.sh
@@ -15,13 +15,13 @@ source ../rma_env/bin/activate # To activate the virtual environment
 # To run the bash script : sbatch scitas_run/classifier.sh (in home/rmarieta/LTS4)
 
 # To train simple models, one in ['bayes','kNN','SVM','tree','logit']
-#python -u classifier/graph_classifier.py --graph_dir './data/v1.5.2/graph_lapl_low_100' --algo 'logit' --seizure_types 'FNSZ' 'GNSZ' --cross_val 'False' --is_cov 'False' --over_conn 'True'
+#python -u classifier/graph_classifier.py --graph_dir './data/v1.5.2/graph_lapl_low_50' --algo 'logit' --seizure_types 'FNSZ' 'GNSZ' --cross_val 'False' --is_cov 'False' --over_conn 'True'
 
 # To train the FC_NN :
-#python -u classifier/FC_NN.py --input_dir './data/v1.5.2/graph_lapl_low_100' --nb_epochs 50 --save_model 'False' --batch_size 30 --over_conn 'True' --save_model 'False'
+#python -u classifier/FC_NN.py --input_dir './data/v1.5.2/graph_lapl_low_50' --nb_epochs 50 --save_model 'False' --batch_size 30 --over_conn 'True' --save_model 'False'
 
 # To train the CNN :
-#python -u classifier/CNN.py --input_dir './data/v1.5.2/graph_lapl_low_100' --nb_epochs 50 --save_model 'False' --batch_size 30 --upper 'True' --is_cov 'False' --revert 'False' --over_conn 'True'
+#python -u classifier/CNN.py --input_dir './data/v1.5.2/graph_lapl_low_50' --nb_epochs 50 --save_model 'False' --batch_size 30 --upper 'True' --is_cov 'False' --revert 'False' --over_conn 'True'
 
 # To train the dual CNN :
 #python -u classifier/dual_CNN.py --input_cov './data/v1.5.2/graph_cov_low_100' --input_lapl './data/v1.5.2/graph_lapl_low_50' --nb_epochs 50 --save_model 'False' --upper 'True' --revert 'False' --batch_size 10 --over_conn 'True'
